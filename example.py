8977ae87b710c7217e34c309701fe53c2d0a5d56
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-11-01 16:54:31 -0500

    example with OAuth

--- example.py
+++ example.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
 
-
+import os
 from restclient import RestClient
 
 
-# You could also pass OAuth in the constructor
-c = RestClient("http://api.metagenomics.anl.gov")
+# Example constructor with OAuth
+c = RestClient("http://api.metagenomics.anl.gov", headers = { "Authorization" : "mgrast "+os.environ['MGRKEY'] })
 
-print("-------------\n")
+
+# Example 1
 response = c.get("/")
 print(response.json())
 
 
-print("-------------\n")
+# Example 2
 response = c.get("/metagenome", params={"verbosity": "minimal"})
 print(response.json())
 
 
-print("-------------\n")
-for elem in c.get_stream("/metagenome", params={"verbosity": "minimal"}):
+
+print("------------------------------\n")
+
+# Example 3: with pagination
+
+params={"verbosity": "minimal"}
+count = 0
+for elem in c.get_stream("/metagenome", limit=5, params=params):
     print(elem)
+    count +=1
+    if count >= 10:
+        break
+
+
