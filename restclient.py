18b7dc4a4c9b37355f86be68ad66eeca07620a0a
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-11-02 15:19:32 -0500

    added delete

--- restclient.py
+++ restclient.py
@@ -9,14 +9,29 @@ class RestClient:
         self.url = url
         self.headers = headers
 
-    def get(self, path="/", headers=None, params=None):
+    def get(self, path="/", headers=None, params=None, debug=False):
+        if debug:
+            print("GET %s/%s\n" % (self.url, path))
         r = requests.get(self.url+"/"+path, headers=headers, params=params)
+        if debug:
+            print("GET %s\n" % (r.url))
+        return r
+    
+    def delete(self, path="/", headers=None, params=None, debug=False):
+        if debug:
+            print("DELETE %s/%s\n" % (self.url, path))
+        r = requests.delete(self.url+"/"+path, headers=headers, params=params)
+        if debug:
+            print("DELETE %s\n" % (r.url))
         return r
         
     # get function that handles REST pagination
-    def get_stream(self, path, headers=None, params=None, offset=0, end=None, limit = 50):
+    def get_stream(self, path, headers=None, params=None, offset=0, end=None, limit=50, debug=False):
         position = offset
     
+        if params == None:
+            params={}
+            
         # merge values
         if self.headers != None:
             if headers == None:
@@ -30,7 +45,7 @@ class RestClient:
         
             params['limit'] = limit
             params['offset'] = offset
-            results = self.get(path, params=params, headers = headers)
+            results = self.get(path, params=params, headers = headers, debug=debug)
             obj = results.json()
             if not "data" in obj:
                 break
