4be79302d9fe44edf9ba1fbb15f7cd015c66faea
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-11-03 16:19:52 -0500

    show creation_month expiration_month and combination

--- find_old_inbox_nodes.py
+++ find_old_inbox_nodes.py
@@ -8,40 +8,68 @@ from restclient import RestClient
 # Example constructor with OAuth
 c = RestClient("http://shock.metagenomics.anl.gov", headers = { "Authorization" : "mgrast "+os.environ['MGRKEY'] })
 
-params={"querynode":1, "type": "basic", "attributes.type" : "inbox"}
+params={"querynode":1, "attributes.type" : "inbox"} #, 'attributes.expiration' : "0001-01-01T00:00:00Z"
 
-total_size = {}
+creation_total_size = {}
+expiration_total_size = {}
+
+mix_total_size = {}
+
 combined_size = 0
 count =0
+
+delete_nodes=[]
+
 for elem in c.get_stream("/node", limit=100, params=params):
     #print(elem)
     
     creation_month = elem['created_on'][:7]
+    expiration_month = elem['expiration'][:7]
     #print("creation_month: "+creation_month)
     
-    if not creation_month in total_size:
-        total_size[creation_month]=0
+    mix  = creation_month+"_"+expiration_month
     
-    if  elem['expiration'] == "0001-01-01T00:00:00Z":
-        size = elem['file']['size']
-        if creation_month.startswith('2016') or creation_month.startswith('2015'):
-            if not creation_month.startswith('2016-1'):
-                print(elem['id'])
-                result = c.delete("/node/"+elem['id'])
-                print(result.text)
-                #print(elem)
-            #print("%s      %s      %d" %(elem['created_on'], elem['expiration'], size))
+    if not mix in mix_total_size:
+        mix_total_size[mix]=0
+    
+    if not creation_month in creation_total_size:
+        creation_total_size[creation_month]=0
+    
+    if not expiration_month in expiration_total_size:
+        expiration_total_size[expiration_month]=0
+    
+    #if creation_month.startswith('2016-10') and expiration_month.startswith('0001-01'):
+        #delete_nodes.append(elem['id'])
         
-        total_size[creation_month] += size
-        combined_size += size
+    size = elem['file']['size']
+  
+    
+    creation_total_size[creation_month] += size
+    expiration_total_size[expiration_month] += size
+    mix_total_size[mix] += size
+    
+    
+    combined_size += size
     count +=1
     if count % 1000 == 0:
-        print(json.dumps(total_size, indent=4, sort_keys=True))
+        print(json.dumps(expiration_total_size, indent=4, sort_keys=True))
     #count >= 1000:
     #    break
 
 #print("total_size: %d MB" % (total_size /1000000))
+print("creation_total_size\n")
+print(json.dumps(creation_total_size, indent=4, sort_keys=True))
 
-print(json.dumps(total_size, indent=4, sort_keys=True))
+print("expiration_total_size\n")
+print(json.dumps(expiration_total_size, indent=4, sort_keys=True))
+
+print("mix_total_size\n")
+print(json.dumps(mix_total_size, indent=4, sort_keys=True))
 
 print("combined_size: %d GB" % (combined_size / 1000000000))
+print("\n")
+print(delete_nodes)
+print("\n")
+for node_id in delete_nodes:
+    result = c.delete("/node/"+node_id, debug=True)
+    print(result.text)
