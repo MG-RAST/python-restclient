e3e525e63748695e4529cad311c20109ba8c9cca
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-11-03 17:31:49 -0500

    improved breakdown

--- shock-inventory.py
+++ shock-inventory.py
@@ -14,23 +14,40 @@ count = 0
 
 inventory={}
 inventory['basic']={}
-inventory['basic']['non-virtual']={}
-inventory['basic']['virtual']={}
-inventory['basic']['non-virtual']['count']=0
-inventory['basic']['non-virtual']['size']=0
-inventory['basic']['non-virtual']['types']={}
+inventory['basic']={}
+inventory['basic']={}
+inventory['basic']['count']=0
+inventory['basic']['size']=0
+inventory['basic']['types']={}
 
-inventory['basic']['virtual']['count']=0
-inventory['basic']['virtual']['size']=0
-inventory['basic']['virtual']['types']={}
+inventory['basic']['count']=0
+inventory['basic']['size']=0
+inventory['basic']['types']={}
 
 for node in c.get_stream("/node", limit=100):
     count +=1
     
-    if count % 10 == 0:
-        print("count: %d" % (count))
+    #if count % 10 == 0:
+    #    print("count: %d" % (count))
    
     node_type = node['type']
+
+        
+    if node_type == "parts":
+        created_on = node["created_on"]
+        created_on_month = created_on[:7]
+        if not node_type in inventory:
+            inventory[node_type]={}
+        
+        if not created_on_month in inventory[node_type]:
+            inventory[node_type][created_on_month]={}
+            inventory[node_type][created_on_month]['count']=0
+            
+            
+        inventory[node_type][created_on_month]['count']+=1
+       
+        continue
+    
     if node_type != "basic":
         if not node_type in inventory:
             inventory[node_type]=0
@@ -38,7 +55,7 @@ for node in c.get_stream("/node", limit=100):
         inventory[node_type]+=1
         continue
    
-   
+    
     
     if not "file" in node:
         print(node)
@@ -47,20 +64,15 @@ for node in c.get_stream("/node", limit=100):
     node_file = node['file']
     
    
-    
-    
     size = 0
     if "size" in node_file:
         size = node_file["size"]
     
-    virtual = False
-    if 'virtual' in node and node['virtual']:
-         virtual = True
     
-    virtual_key = "virtual"
-         
-    if not virtual:
-        virtual_key = 'non-virtual'
+    
+    
+    
+   
         
     if "attributes" in node:
         
@@ -72,26 +84,54 @@ for node in c.get_stream("/node", limit=100):
             if "type" in attributes:
                 node_attr_type = attributes["type"]
         
-        if not node_attr_type in inventory['basic'][virtual_key]['types']:
-            inventory['basic'][virtual_key]['types'][node_attr_type]= {}
-            inventory['basic'][virtual_key]['types'][node_attr_type]['count']=0
-            inventory['basic'][virtual_key]['types'][node_attr_type]['size']=0
+        if not node_attr_type in inventory['basic']['types']:
+            inventory['basic']['types'][node_attr_type]= {}
+            inventory['basic']['types'][node_attr_type]['count']=0
+            inventory['basic']['types'][node_attr_type]['size']=0
             
             
-        inventory['basic'][virtual_key]['types'][node_attr_type]['count'] += 1
-        inventory['basic'][virtual_key]['types'][node_attr_type]['size'] += size
+        inventory['basic']['types'][node_attr_type]['count'] += 1
+        inventory['basic']['types'][node_attr_type]['size'] += size
         
-    inventory['basic'][virtual_key]['count'] += 1
-    inventory['basic'][virtual_key]['size'] += size
+        if node_attr_type == "metagenome":
+            data_type = "unknown"
+            if "data_type" in attributes:
+                data_type = attributes["data_type"]
+            
+            if not "data_types" in inventory['basic']['types'][node_attr_type]:
+                inventory['basic']['types'][node_attr_type]["data_types"]={}
+            
+            if not data_type in inventory['basic']['types'][node_attr_type]["data_types"]:
+                inventory['basic']['types'][node_attr_type]["data_types"][data_type]={}
+                inventory['basic']['types'][node_attr_type]["data_types"][data_type]['count']=0
+                inventory['basic']['types'][node_attr_type]["data_types"][data_type]['size']=0
+                
+            inventory['basic']['types'][node_attr_type]["data_types"][data_type]['count']+=1
+            inventory['basic']['types'][node_attr_type]["data_types"][data_type]['size']+=size
+
+        
+    inventory['basic']['count'] += 1
+    inventory['basic']['size'] += size
         
 
         
-    if count % 100 == 0:
+    if count % 500 == 0:
+        print("count: %d" % (count))
         print(json.dumps(inventory, indent=4, sort_keys=True))
-
+        print("\ndata_type\tcount\tsize(GiB)")
+        for data_type in inventory['basic']['types']['metagenome']["data_types"]:
+            data_type_obj= inventory['basic']['types']['metagenome']["data_types"][data_type]
+            
+            print("metagenome_%s\t%d\t%d" % (data_type , data_type_obj['count'], data_type_obj['size']/1073741824 ))
+        for basic_type in inventory['basic']['types']:
+            if basic_type != "metagenome":
+                basic_type_obj = inventory['basic']['types'][basic_type]
+                print("%s\t%d\t%d" % (basic_type , basic_type_obj['count'], basic_type_obj['size']/1073741824 ))
+                
+                
 
 print(json.dumps(inventory, indent=4, sort_keys=True))
 
-mb = inventory['basic']['non-virtual']['size'] / 1000000
+mb = inventory['basic']['size'] / 1000000
 
 print("\nmegabyte: %d" % (mb))
