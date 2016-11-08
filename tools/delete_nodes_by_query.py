b4531febfc5ad20f5b2d5ed709ed492510ad4a23
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-11-08 16:43:33 -0600

    delete old empty nodes

--- tools/delete_nodes_by_query.py
+++ tools/delete_nodes_by_query.py
@@ -2,6 +2,8 @@
 
 
 import os
+import sys
+sys.path.insert(0, "..")
 from restclient import RestClient
 
 
@@ -9,13 +11,54 @@ c = RestClient("http://shock.metagenomics.anl.gov", headers = { "Authorization" 
 
 delete_nodes=[]
 
-params={"query":1 , "type":"metagenome" , "data_type" : "profile"}
+#http://shock.metagenomics.anl.gov/node?querynode&owner=f6b96a9e-471b-4a24-beb4-840472b33a5a&attributes=null&offset=100000&type=basic&direction=asc
+params={"querynode":1 , "owner":"f6b96a9e-471b-4a24-beb4-840472b33a5a" , "attributes" : "null", "type":"basic", "file.size":"0", "direction" : "asc"}
+#params={"query":1 , "type":"metagenome" , "data_type" : "profile"}
 
 count=0
-for node in c.get_stream("/node", limit=100, params=params):
+for node in c.get_stream("/node", limit=1000, params=params, debug=True):
     count += 1
+    
+    if node['file']['size'] > 0:
+        continue
+        
+    if 'attributes' in node:
+        leave = False
+        leave_reason="none"
+        attributes = node['attributes']
+        if attributes != None:
+            if type(attributes) is str:
+                attributes_str = attributes
+                if not attributes_str == 'None':
+                    print("attributes_str: %s" % (attributes_str))
+                    leave = True
+                    leave_reason="is a string, but not None (%s)" % (attributes_str)
+            
+            else:
+                print("attributes not str")
+                leave = True
+                leave_reason="something other than string (%s)" % (str(type(attributes)))
+            
+            if leave:
+                print(node)
+                print("found attributes")
+                print("leave_reason: %s" % (leave_reason))
+                print(attributes)
+                sys.exit(0)
+    
+    created_on = node["created_on"]
+    #created_on_month = created_on[:7]
+    created_on_year = int(created_on[:4])
+    
+    if created_on_year > 2014:
+        break
+    
     print(node)
-    delete_nodes.append(node['id'])
+    if created_on_year < 2016:
+        print("delete old %d" % (created_on_year))
+        delete_nodes.append(node['id'])
+    else:
+        print("keep new")
 
 print("count: %d" % (count))
 
